8e723208fec4549dd89142f75f2df5257efdfc71
Elad Steinmetz <Elad Steinmetz@YGGDRASIL>
2023-03-24 14:31:42 +0300

    Under construction site

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This GH Pages website is under construction.
+          After we have an idea of what to put here, we will.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
